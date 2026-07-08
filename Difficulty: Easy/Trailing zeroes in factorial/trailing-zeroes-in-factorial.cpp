7b88bccbd305d816723b2c47c67c11775c14358e
kanchanwar siddhant <siddhantkanchanwar@gmail.com>
2026-07-08 11:51:32 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Trailing zeroes in factorial/trailing-zeroes-in-factorial.cpp
@@ -0,0 +1,15 @@
+class Solution {
+  public:
+    int trailingZeroes(int n) {
+        // code here
+        if(n<5){
+            return 0;
+        }
+        int count =0;
+        while(n>=5){
+            count+=n/5;
+            n = n/5;
+        }
+        return count;
+    }
+};
