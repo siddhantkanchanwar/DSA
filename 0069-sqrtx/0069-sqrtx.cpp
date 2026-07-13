503367606e2814195f0e38ceade432e0f9317b5b
kanchanwar siddhant <siddhantkanchanwar@gmail.com>
2026-07-13 10:56:15 +0530

    Time: 0 ms (100%), Space: 8.6 MB (49.8%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,7 +1,27 @@
 class Solution {
 public:
     int mySqrt(int x) {
-    //  for(int i=1;i)   
-    return pow(x,0.5);
+        long long start=1,end=x,ans;
+        long long mid;
+        if(x==0){
+            return 0;
+        }
+        while(start<=end){
+            mid = start + (end-start)/2;
+            if(mid*mid==x){
+                return mid;
+                break;
+            }
+            else if(mid*mid<x){
+                ans = mid;
+                start = mid+1;
+            }
+            else{
+                // ans = mid;
+                end = mid-1;
+            }
+        }
+        return ans;
+   
     }
 };
