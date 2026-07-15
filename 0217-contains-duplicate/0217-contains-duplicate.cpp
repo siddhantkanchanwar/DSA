a9fac0f89e8552ae828e1ccc2a4a05aa58e6cf19
kanchanwar siddhant <siddhantkanchanwar@gmail.com>
2026-07-15 11:25:38 +0530

    Time: 26 ms (85.55%), Space: 82.8 MB (81.33%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,14 +1,9 @@
 class Solution {
 public:
-    bool containsDuplicate(vector<int>& nums) {
-        // int count =0;
-        sort(nums.begin(),nums.end());
+    bool containsDuplicate(vector<int>& nums){
+         sort(nums.begin(),nums.end());
         for(int i=0;i<nums.size()-1;i++){
             if(nums[i]==nums[i+1]){
                 return 1;
-                break;
-            }
-        }
-        return 0;
-    }
-};
+                break;}}
+        return 0;}};
