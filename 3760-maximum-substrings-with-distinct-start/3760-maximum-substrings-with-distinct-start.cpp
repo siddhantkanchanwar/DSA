2b5586c766a2fbbafbeb22b13a4c81c9ef426a63
kanchanwar siddhant <siddhantkanchanwar@gmail.com>
2026-07-16 12:05:42 +0530

    Time: 7 ms (84.18%), Space: 23.9 MB (82.63%) - LeetHub

--- 3760-maximum-substrings-with-distinct-start/3760-maximum-substrings-with-distinct-start.cpp
+++ 3760-maximum-substrings-with-distinct-start/3760-maximum-substrings-with-distinct-start.cpp
@@ -1,7 +1,17 @@
 class Solution {
 public:
     int maxDistinct(string s) {
-        set<char> se(s.begin(),s.end());
-        return se.size();
+        int count =0;
+        int freq[26]={0};
+        for(auto x:s){
+            if(x>='a' && x<='z'){
+                if(freq[x-'a']<1){
+                    count++;
+                }
+                freq[x-'a']=1;
+            }
+        }
+        return count;
+
     }
 };
