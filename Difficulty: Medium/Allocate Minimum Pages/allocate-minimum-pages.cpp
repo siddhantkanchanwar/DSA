4808b191c58ee22487281aae632ad86620cbf06b
kanchanwar siddhant <siddhantkanchanwar@gmail.com>
2026-07-18 12:09:57 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Allocate Minimum Pages/allocate-minimum-pages.cpp
@@ -0,0 +1,32 @@
+class Solution {
+  public:
+    int findPages(vector<int> &arr, int k) {
+        // code here
+        if(arr.size()<k){
+            return -1;
+        }
+        long long start = *max_element(arr.begin(),arr.end()),end,mid,ans;
+        for(long long i=0;i<arr.size();i++){
+            end += arr[i];
+        }
+        while(start<=end){
+            mid = start+(end-start)/2;
+            long long pages = 0, count=1;
+            for(long long i=0;i<arr.size();i++){
+                pages+=arr[i];
+                if(pages>mid){
+                   count++;
+                   pages = arr[i];
+                }
+            }
+            if(count<=k){
+                ans = mid;
+                end = mid-1;
+            }
+            else{
+                start = mid+1;
+            }
+        }
+        return ans;
+    }
+};
