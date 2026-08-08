10d60eee73f730aff318412a1fc1228652d1a608
kanchanwar siddhant <siddhantkanchanwar@gmail.com>
2026-08-09 02:21:22 +0530

    Time: 0 ms (100%), Space: 21.8 MB (71.45%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -5,7 +5,6 @@ public:
         for(int i=0;i<nums.size();i++){
             if(nums[i]!=i){
                 return i;
-                break;
             }
         }
         return nums.size();
